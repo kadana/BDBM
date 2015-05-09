35a69d43c5ee5b0865fa6342314ff9d7349d5afa
Kanoelani Dana <kanoelanidana@Kanoelanis-MacBook-Pro-6.local>
2015-05-09 09:22:06 -0700

    HTML-CSS Updated Nav

--- _js/author.js
+++ _js/author.js
@@ -4,3 +4,12 @@
 $('.carousel').carousel({
   interval: 2000
 })
+
+
+/*--------------
+| Navigation
+---------------*/
+document.getElementById('IDHERE').innerHTML = 'HTML GOES HERE';
+
+
+document.getElementsByClassName('ClassGoesHERE')[0].innerHTML = 'HTML GOES HERE';
